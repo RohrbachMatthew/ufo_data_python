da6f2989cb41b0622269332977585799b1ac03a0
RohrbachMatthew <matthewrohrbach1091@gmail.com>
2025-04-21 00:11:56 -0400

    Added non us per month df

--- non_us_data.py
+++ non_us_data.py
@@ -1,2 +1,28 @@
 # This file will be for non US data
 # US has most of the data, looking for insights from other countries
+
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+from pandas import DataFrame
+
+from create_df import fetch_data
+
+def prepare_month_non_us():
+    rows, columns = fetch_data()
+    df = DataFrame(rows, columns=columns)
+
+    # Remove NA
+    df.replace('', pd.NA, inplace=True)
+
+    df = df.dropna(subset=['datetime'])
+
+    df = df[df['country'] != 'us']
+
+    df['month'] = pd.to_datetime(df['datetime']).dt.month.astype(int)
+
+    months = df.groupby('month').size().reset_index(name='per_month')
+
+    return months
+
+def month_non_us_plot(month, save=False):
